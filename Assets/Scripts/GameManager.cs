20ff9d5165f39e5c46660dde9d416a3812a77d22
Snowqueen1617 <65308047+Snowqueen1617@users.noreply.github.com>
2020-06-26 22:52:35 -0700

    MS1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            UnityEngine.Debug.LogWarning("Attempted to create a second game manager.");
+            Destroy(this.gameObject);
+        }
+    }
+    public void StartGame()
+    {
+        UnityEngine.Debug.Log("Game has started!");
+    }
+}
---
+++ Assets/Scripts/PercentageBar.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PercentageBar : MonoBehaviour
+{
+    public float currentValue;
+    public float maxValue;
+    private Image imageComponent;
+    public Color barColor = Color.green;
+    public Color barCriticalColor = Color.cyan;
+    public float criticalLevel = 0.25f;
+
+    public Image.FillMethod fillMethod = Image.FillMethod.Horizontal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        imageComponent = gameObject.GetComponent<Image>();
+        if (imageComponent != null)
+        {
+            imageComponent.type = Image.Type.Filled;
+            imageComponent.fillMethod = fillMethod;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float percentOfMax = currentValue / maxValue;
+
+        imageComponent.fillAmount = percentOfMax;
+        if (percentOfMax > criticalLevel)
+        {
+            imageComponent.color = barColor;
+        }
+        else
+        {
+            imageComponent.color = barCriticalColor;
+        }
+    }
+}
