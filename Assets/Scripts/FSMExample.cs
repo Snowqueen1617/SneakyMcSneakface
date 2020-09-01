e01bb59b22da7473af4a203850dc4d70ff7c1036
Sammi Wilcox <samwilco@uat.edu>
2020-09-01 10:25:07 -0700

    9-1-2020

---
+++ Assets/Scripts/FSMExample.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+
+public class FSMExample : MonoBehaviour
+{
+    public string AIState = "Idle";
+    public float aiSenseRadius;
+    public Vector3 targetPosition;
+    public float health;
+    public float healthCutoff;
+    public float moveSpeed;
+    public float healingRate;
+    public float maxHealth;
+    public float hearingDistance;
+    public float fieldOfView = 90f;
+    public float visionMaxDistance = 40f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (AIState == "Idle")
+        {
+            //Do the state behavior
+            Idle();
+            //Check for transitions
+            if(Vector3.Distance(transform.position, targetPosition) < aiSenseRadius)
+            {
+                ChangeState("Seek");
+            }
+        }
+        else if (AIState == "Seek")
+        {
+            //Do the state behavior
+            Seek();
+            //Check for transitions
+            if(health < healthCutoff)
+            {
+                ChangeState("Rest");
+            }
+            else if(Vector3.Distance(transform.position, targetPosition) >= aiSenseRadius)
+            {
+                ChangeState("Idle");
+            }
+        }
+        else if(AIState == "Rest")
+        {
+            //Do the state behavior
+            Rest();
+            //Check for transitions
+            if(health >= healthCutoff)
+            {
+                ChangeState("Idle");
+            }
+        }
+        else
+        {
+            Debug.LogWarning("AIState not found: " + AIState);
+        }
+    }
+
+    void Idle()
+    {
+        //Do Nothing
+    }
+
+    void Seek()
+    {
+        Vector3 vectorToTarget = targetPosition - transform.position;
+        transform.position += vectorToTarget.normalized * moveSpeed * Time.deltaTime;
+    }
+
+    void Rest()
+    {
+        //TO DO: Write the Rest State
+        health += healingRate * Time.deltaTime;
+        health = Mathf.Min(health, maxHealth);
+    }
+
+    public void ChangeState(string newState)
+    {
+        AIState = newState;
+    }
+
+    private bool CanHear(GameObject target)
+    {
+        //Get the target's noise maker
+        NoiseMaker targetNoiseMaker = target.GetComponent<NoiseMaker>();
+
+        //If they don't have a nise maker, we can't hear them
+        if(targetNoiseMaker == null) { return false; }
+
+        //If the distance between us and the target is less than the sum of the noise distance and hearing distance, we can hear it
+        float distanceToTarget = Vector3.Distance(transform.position, target.transform.position);
+        if((targetNoiseMaker.volumeDistance + hearingDistance) > distanceToTarget) { return true; }
+
+        return false;
+    }
+
+    private bool CanSee(GameObject target)
+    {
+        Vector3 vectorToTarget = target.transform.position - transform.position;
+
+        float angleToTarget = Vector3.Angle(vectorToTarget, transform.up);
+
+        //Check if the target is within the field of view.
+        if(angleToTarget < fieldOfView)
+        {
+            //Use a raycast to see if there are obstuctions between us and the target 
+            RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, vectorToTarget, visionMaxDistance);
+
+            if(hitInfo.collider.gameObject == target)
+            {
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,17 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public string gameState = "Start Screen";
+
     private static GameManager instance;
+    public GameObject titleScreen;
+    public GameObject gameUI;
+    public GameObject player;
+    public GameObject playerPrefab;
+    public GameObject playerSpawnPoint;
+    public int lives = 3;
+    public GameObject playerDeathScreen;
+    public GameObject gameOverScreen;
     public static GameManager Instance
     {
         get
@@ -27,8 +37,129 @@ public class GameManager : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
+
+    public void ChangeState(string newState)
+    {
+        gameState = newState;
+    }
     public void StartGame()
     {
         UnityEngine.Debug.Log("Game has started!");
+        ChangeState("Initialize Game");
+    }
+
+    private void Update()
+    {
+        if(gameState == "Start Screen")
+        {
+            //Do the state behavior
+            StartScreen();
+            //Check for transitions
+            //This transitions on a button click
+        }
+        else if(gameState == "Initialize Game")
+        {
+            //Do the state behavior
+            InitializeGame();
+            //Check for transitions
+            ChangeState("Spawn Player");
+        }
+        else if(gameState == "Spawn Player")
+        {
+            //Do the state behavior
+            SpawnPlayer();
+            //Check for transitions
+            ChangeState("Gameplay");
+        }
+        else if(gameState == "Gameplay")
+        {
+            //Do the state behavior
+            Gameplay();
+            //Check for transitions
+            if (player == null && lives > 0)
+            {
+                ChangeState("Player Death");
+            }
+            else if (player == null && lives <= 0)
+            {
+                ChangeState("Game Over");
+            }
+        }
+        else if(gameState == "Player Death")
+        {
+            //Do the state behavior
+            PlayerDeath();
+            //Check for transitions
+            if(Input.anyKeyDown)
+            {
+                ChangeState("Spawn Player");
+            }
+        }
+        else if(gameState == "Game Over")
+        {
+            //Do the state behavior
+            GameOver();
+            //Check for transitions
+            if(Input.anyKeyDown)
+            {
+                ChangeState("Start Screen");
+            }
+        }
+        else
+        {
+            UnityEngine.Debug.LogWarning("Game Manager tried to change to non-existant state");
+        }
+    }
+
+    public void StartScreen()
+    {
+        //Show the menu
+        if(!titleScreen.activeSelf)
+        {
+            titleScreen.SetActive(true);
+        }
+ 
+    }
+
+    public void InitializeGame()
+    {
+        //Reset all variables
+        //TODO: Reset variables in initialization
+        //Turn off menu
+        titleScreen.SetActive(false);
+        //Turn on game ui
+        gameUI.SetActive(true);
+    }
+
+    public void SpawnPlayer()
+    {
+        //Add the player to the world
+        player = Instantiate(playerPrefab, playerSpawnPoint.transform.position, Quaternion.identity);
+    }
+
+    public void Gameplay()
+    {
+       
+    }
+
+    public void PlayerDeath()
+    {
+
+        if(!playerDeathScreen.activeSelf)
+        {
+            playerDeathScreen.SetActive(true);
+        }
+    }
+
+    public void GameOver()
+    {
+        if(gameUI.activeSelf)
+        {
+            gameUI.SetActive(false);
+        }
+        if(!gameOverScreen.activeSelf)
+        {
+            gameOverScreen.SetActive(true);
+        }
     }
 }
