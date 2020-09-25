7446d4e190b3e4e48825873b64e3e4f14cf69001
Sammi Wilcox <samwilco@uat.edu>
2020-09-24 18:09:34 -0700

    9-24-20

--- Assets/Scripts/FSMExample.cs
+++ Assets/Scripts/FSMExample.cs
@@ -14,12 +14,6 @@ public class FSMExample : MonoBehaviour
     public float fieldOfView = 90f;
     public float visionMaxDistance = 40f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
